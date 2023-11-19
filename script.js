b78e7e5903d2c679086fc05091f4ec940815bfe3
gemlsmusic <134534839+gemlsmusic@users.noreply.github.com>
2023-11-19 18:52:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,96 @@
 jQuery(document).ready(function() {
-   
-    
-	jQuery('#play').on('click', function() {
-		$('iframe').attr("src", "https://www.youtube.com/embed/jfKfPfyJRdk?controls=0&autoplay=1");
-	});
-    jQuery('#stop').on('click', function() {
-		$('iframe').attr("src", "");
-	});
-    jQuery('#hide').on('click', function() {
-        if($('iframe').css('display') == 'inline') {
-            $('iframe').css('display','none');
-            $('#hide').text('unhide');
-        } else {
-            $('iframe').removeAttr('style');
-            $('#hide').text('hide');
-        }
+	let audioElement = jQuery('audio')[0];
+
+	audioElement.addEventListener('ended', function() {
+		jQuery('#pause').css('display','none');
+		jQuery('#loading_track').css('display', 'flex');
+        jQuery.ajax({
+			url: `https://fdnbzdngfznyxfmxzdtnedyrj.omoeahasva.repl.co/track`,
+			method: 'get',
+			success: function(data) {
+				console.log(data);
+				jQuery('audio').attr('src',data['url_audio']);
+				jQuery('.title').html(data['title']);
+				jQuery('.duration').html(`0m0s/${data['duration_str']}`);
+				audioElement.play();
+				audioElement.addEventListener("timeupdate",function(){
+					jQuery(".duration").text(`${Math.floor(audioElement.currentTime / 60)}m${Math.floor(audioElement.currentTime % 60)}s/${data['duration_str']}`);
+					jQuery('.progress').attr('aria-valuenow', audioElement.currentTime * 100 / audioElement.duration);
+                    jQuery('.progress-bar').css('width', audioElement.currentTime * 100 / audioElement.duration + '%');
+				});
+				jQuery('#pause').css('display','flex');
+				jQuery('#loading_track').css('display', 'none');
+			},
+			error: function () {
+				alert('error');
+			}
+		});
+    }, false);
+
+    jQuery('.next').on('click', function() {
+    	audioElement.pause();
+    	jQuery('#pause').css('display','none');
+		jQuery('#loading_track').css('display', 'flex');
+    	jQuery.ajax({
+			url: `https://fdnbzdngfznyxfmxzdtnedyrj.omoeahasva.repl.co/track`,
+			method: 'get',
+			success: function(data) {
+				console.log(data);
+				jQuery('audio').attr('src',data['url_audio']);
+				jQuery('.title').html(data['title']);
+				jQuery('.duration').html(`0m0s/${data['duration_str']}`);
+				audioElement.play();
+				audioElement.addEventListener("timeupdate",function(){
+					jQuery(".duration").text(`${Math.floor(audioElement.currentTime / 60)}m${Math.floor(audioElement.currentTime % 60)}s/${data['duration_str']}`);
+					jQuery('.progress').attr('aria-valuenow', audioElement.currentTime * 100 / audioElement.duration);
+                    jQuery('.progress-bar').css('width', audioElement.currentTime * 100 / audioElement.duration + '%');
+				});
+				jQuery('#pause').css('display','flex');
+				jQuery('#loading_track').css('display', 'none');
+			},
+			error: function () {
+				alert('error');
+			}
+		});
+    });
+
+	jQuery('#pause').on('click', function() {
+		audioElement.pause();
+		jQuery('#pause').css('display','none');
+		jQuery('#play').css('display','flex');
 	});
 
+	jQuery('#play').on('click', function() {
+		if(jQuery('audio').attr('src') != undefined) {
+			audioElement.play();
+			jQuery('#pause').css('display','flex');
+			jQuery('#play').css('display','none');
+
+		} else {
+			jQuery('#loading_track').css('display', 'flex');
+			jQuery('#play').css('display', 'none');
+			jQuery.ajax({
+				url: `https://fdnbzdngfznyxfmxzdtnedyrj.omoeahasva.repl.co/track`,
+				method: 'get',
+				success: function(data) {
+					console.log(data);
+					jQuery('audio').attr('src',data['url_audio']);
+					jQuery('.title').html(data['title']);
+					jQuery('.duration').html(`0m0s/${data['duration_str']}`);
+					jQuery('#loading_track').css('display', 'none');
+					jQuery('#pause').css('display','flex');
+					jQuery('#play').css('display','none');
+					audioElement.play();
+					audioElement.addEventListener("timeupdate",function(){
+						jQuery(".duration").text(`${Math.floor(audioElement.currentTime / 60)}m${Math.floor(audioElement.currentTime % 60)}s/${data['duration_str']}`);
+						jQuery('.progress').attr('aria-valuenow', audioElement.currentTime * 100 / audioElement.duration);
+                        jQuery('.progress-bar').css('width', audioElement.currentTime * 100 / audioElement.duration + '%');
+					});
+				},
+				error: function () {
+					alert('error');
+				}
+			});
+			}
+	});
 });
